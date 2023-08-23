f87a989dc85027e2d187d535d1b3988b7b35a9c0
andreluiz1982 <andreguella@gmail.com>
2023-08-23 11:29:11 -0300

    melhorando html

--- src/main/resources/templates/static/aliquota.js
+++ src/main/resources/templates/static/aliquota.js
@@ -3,59 +3,35 @@ let valorMaximo = document.getElementById("valorMaximo");
 let aliquota = document.getElementById("aliquota");
 let anoMes = document.getElementById("anoMes");
 let faixaAliquota = document.getElementById("faixaAliquota");
-listener(aliquota);
 let form = document.getElementById("form");
-
-function listener(html) {
-
-	html.addEventListener('keypress', (event) => {
-
-		let keyCode = event.key;
-		addFaixasHtml(keyCode);
-
-
-});
-	
-} 
-function addFaixasHtml(keyCode){
-	
-	if (keyCode === "Enter") {
-        let faixa =  form.appendChild(faixaAliquota.cloneNode(true))
-        let inputs = faixa.getElementsByTagName('input');
-          for (index = 0; index < inputs.length; ++index) {
-          inputs[index].id = "faixaAliquota"+index;
-            	    if(inputs[index].type =="text")
-                    inputs[index].value = '';
-                }
-		listener( form.lastElementChild.lastElementChild)
-
-
-	}
-}
+let response = document.getElementById("response");
+loadAliquotas();
 
 function enviar() {
 	
 	let faixas = [];
-	let f = document.getElementsByClassName("faixa m-3 p-3 border border-info rounded");
-	console.log('size  '+ f.length)
-	for(let i in f){
-	console.log('i ' + i)
-	if(i ==1 ){
-	    let min = f[i].querySelector('#valorMinimo').value;
-	    let max = f[i].querySelector('#valorMaximo').value;
-	    let aliq = f[i].querySelector('#aliquota').value;
+	for(let i = 1; i <= 4; i++){
+	    let f = document.getElementById('faixaAliquota'+i);
 
-	    console.log(min + ' '+ max + ' ' + aliq)
-	    faixas += {
+	    let min = document.getElementById('valorMinimo'+i).value;
+	    let max = document.getElementById('valorMaximo'+i).value;
+	    let aliq = document.getElementById('aliquota'+i).value;
+
+	    if(min != '' && max != '' && aliq != ''){
+
+	    faixas.push( {
 	        'valorMinimo': min,
 	        'valorMaximo':max,
 	        'aliquota': aliq
+	    });
 	    }
-	    console.log(faixas)
+
+
 	}
-	}
+
+
 	let json = JSON.stringify({
-		anoMes: anoMes.value, faixasAliquotas: [faixas]
+		anoMes: anoMes.value, faixasAliquotas: faixas
 	});
 	console.log(JSON.parse(json))
 	var xhr = new XMLHttpRequest();
@@ -64,10 +40,42 @@ function enviar() {
 	xhr.onload = function() {
 		let resp = JSON.parse(this.response);
 		console.log(JSON.parse(this.response).nomeCompleto);
-		window.alert(`Aliquota do mes ${resp.anoMes}, percentual ${resp.aliquota}, 
+		window.alert(`Aliquota do mes ${resp.anoMes}, percentual ${resp.aliquota},
 			valor mínimo ${resp.valorMinimo} e valor máximo ${resp.valorMaximo}, salva com sucesso.`)
 	};
 	xhr.send(json);
-	//location.reload();
+	loadAliquotas();
+	location.reload();
+
+}
+
+function loadAliquotas(){
+    let htmlResp =  `<table><thead><tr><th>Ano-Mês</th><th>Mín.</th><th>Máx</th><th>Aliq.</th></tr></thead><tbody>`;
+    var xhr = new XMLHttpRequest();
+	xhr.open("GET", "/aliquota/all", true)
+	xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xhr.onload = function() {
+		let resp = JSON.parse(this.response);
+		for(let i in resp){
+		    let anoMes = resp[i].anoMes;
+		    let fx =  resp[i].faixasAliquotas;
+        for(let f in fx){
+            htmlResp += `
+                <tr>
+                    <td>${anoMes}</td>
+                    <td>${fx[f].valorMinimo}</td>
+                    <td>${fx[f].valorMaximo}</td>
+                    <td>${fx[f].aliquota}</td>
+                </tr>
+
+            `;
+        }
+
+
+		}
+		htmlResp += `</tbody></table>`;
+		response.innerHTML = htmlResp;
+	};
+	xhr.send();
 
 }
